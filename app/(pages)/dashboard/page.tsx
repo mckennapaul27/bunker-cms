0f282f6423ee746e7c0558357e7517f5fb823815
Paul McKenna <mckennapaul27@gmail.com>
2024-01-26 14:28:44 +0000

    uncommented out sign up page

---
+++ app/(pages)/dashboard/page.tsx
@@ -0,0 +1,54 @@
+import { StickyHeader } from '@/components/collections/StickyHeader';
+import { DashboardWrapper } from '@/components/dashboard/DashboardWrapper';
+import { collection_models } from '@/models/models';
+import styles from '../../../styles/dashboard-general.module.scss';
+import classNames from 'classnames';
+import Link from 'next/link';
+import { FilePlus } from 'lucide-react';
+
+export default function Page({}) {
+    return (
+        <DashboardWrapper>
+            <StickyHeader>
+                <div className={'sticky-header__collection-details'}>
+                    <h1>Dashboard home</h1>
+                    <p>Manage your content</p>
+                </div>
+                <div className={'actions'}>
+                    <Link
+                        href={'/dashboard/content-manager/tag/create'}
+                        className="button small"
+                    >
+                        Create new entry
+                    </Link>
+                </div>
+            </StickyHeader>
+
+            <div className={styles['collections-boxes']}>
+                {collection_models.map((model, i) => {
+                    return (
+                        <div
+                            className={styles['collection-wrapper']}
+                            key={`content-model-${i}`}
+                        >
+                            <Link
+                                href={model.slug}
+                                key={model.name}
+                                className={styles['collection-link']}
+                            >
+                                <span
+                                    className={styles['collection-svg-wrapper']}
+                                >
+                                    <FilePlus />
+                                </span>
+                                <span className={styles['collection-name']}>
+                                    {model.name}
+                                </span>
+                            </Link>
+                        </div>
+                    );
+                })}
+            </div>
+        </DashboardWrapper>
+    );
+}
